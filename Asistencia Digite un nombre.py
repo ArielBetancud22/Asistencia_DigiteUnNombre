d42a7e381b27d4057cc3414c10493ddfcbe47d53
LuchoDiazz <111991734+LuchoDiazz@users.noreply.github.com>
2022-10-19 10:43:53 -0300

    Update Asistencia Digite un nombre.py

--- Asistencia Digite un nombre.py
+++ Asistencia Digite un nombre.py
@@ -19,3 +19,19 @@ alto = int(input("ingrese el alto"))
 profundidad = int(input("ingrese la profundidad"))
 Cubo = Cubo(alto, ancho, profundidad)
 print(F"El volumen del cubo es: {Cubo.calcular_volumen()}")
+
+
+#Ejercicio realizado por Luciano Diaz
+#Crear una funcion para calcular el total de un pago inluyendo impuesto IVA
+
+def calcular_total_pago(pago_sin_impuesto, impuesto):
+    pago_total = pago_sin_impuesto + pago_sin_impuesto * (impuesto/100)
+    
+    return pago_total
+#Ejecutamos la funcion
+
+pago_sin_impuesto = float(input("digite el pago sin impuestos: "))
+impuesto = float(input("Digite el monto del impuesto a apgar: "))
+pago_con_inpuesto = calcular_total_pago (pago_sin_impuesto, impuesto)
+
+print("El pago con impuesto es de ; {pago_con_impuesto}")
