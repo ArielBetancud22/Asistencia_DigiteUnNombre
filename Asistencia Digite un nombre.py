6c48206cdeec67d48f67b1a51de84dd945458b2d
Carolina Jurczyszyn <112590235+Jurcaro@users.noreply.github.com>
2022-11-09 10:47:34 -0300

    Update Asistencia Digite un nombre.py
    Agrego asistencia Carolina

--- Asistencia Digite un nombre.py
+++ Asistencia Digite un nombre.py
@@ -35,3 +35,39 @@ impuesto = float(input("Digite el monto del impuesto a apgar: "))
 pago_con_impuesto = calcular_total_pago (pago_sin_impuesto, impuesto)
 
 print(f"El pago con impuesto es de ; {pago_con_impuesto}")
+
+
+#Ejercicio realizado por Carolina Jurczysyzn
+
+from Color import Color
+from FiguraGeometrica import FiguraGeometrica
+
+class Rectangulo(FiguraGeometrica, Color):
+    def __init__(self, alto, ancho, color):
+        FiguraGeometrica.__init__(self, ancho, alto)
+        Color.__init__(self, color)
+
+    def calcular_area(self):
+        return self.alto * self.ancho
+
+    def __str__(self):
+        return f'{FiguraGeometrica.__str__(self)}, {Color.__str__(self)}'
+
+    #Ejecutamos desde test figura geométrica
+from Cuadrado import Cuadrado
+from Rectangulo import Rectangulo
+
+cuadrado1 = Cuadrado(5, 'Rojo')
+print(cuadrado1.ancho)
+print(cuadrado1.alto)
+print(f'Cálculo del área del cuadrado: {cuadrado1.calcular_area()}')
+
+# MRO
+print(Cuadrado.mro())
+
+print(cuadrado1)
+
+rectangulo1 = Rectangulo(3, 6, 'Verde')
+print(f'Cálculo del área del rectángulo: {rectangulo1.calcular_area()}')
+print(rectangulo1)
+
