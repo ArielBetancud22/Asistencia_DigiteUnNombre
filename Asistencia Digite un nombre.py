fb93f7c5b3a5b3d6142b110ad51ce655c72ad4ea
Flor-balladares <111933306+Flor-balladares@users.noreply.github.com>
2022-11-10 20:31:41 -0300

    Update Asistencia Digite un nombre.py

--- Asistencia Digite un nombre.py
+++ Asistencia Digite un nombre.py
@@ -110,3 +110,29 @@ print(f"La resta de los numeros es: {aritmetica1.resta()}")
 print(f"La multiplicacion de los numeros es: {aritmetica1.multiplicar()}")
 print(f"La division de los numeros es: {aritmetica1.dividir():.2f}")
 
+
+#Ejercicio realizado por Balladares Florencia
+# Ejercicio 3: Funcion Recursiva
+# Imprimir numero de 5 a 1 de manera descente usando funciones recursivas
+# Puede ser cualquier valor positivo, por ejemplo, si pasamos el
+# valor de 5, debe imprimir:
+# 5
+# 4
+# 3
+# 2
+# 1
+# En caso de ser el numero 3 debe imprimir:
+# 3
+# 2
+# 1
+# Si se ingresan numeros negativos no imprime nada
+def imprimir_numeros_recursivos(numero):
+    if numero >= 1:
+        print(numero)
+        imprimir_numeros_recursivos(numero-1) #Caso recursivo
+    elif numero == 0:
+        return
+    elif numero <= 0:
+        print("valor ingresado incorrecto...")
+
+imprimir_numeros_recursivos(5)
