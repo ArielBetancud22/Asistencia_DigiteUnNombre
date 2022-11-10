a1e8b41f27dc2c85b8a741f5adad5ce99e693ccd
myri007 <111933095+myri007@users.noreply.github.com>
2022-11-10 14:44:10 -0300

    Update Asistencia Digite un nombre.py

--- Asistencia Digite un nombre.py
+++ Asistencia Digite un nombre.py
@@ -79,3 +79,34 @@ var nombreCompleto =  nombre+" "+apellido;
 console.log(nombreCompleto);
 var nombreCompleto2 = "Juan"+" "+"Eztala";
 console.log(nombreCompleto2); 
+
+
+# Ejercicio realizado por Myriam Nuñez 
+class Aritmetica:
+    """
+    Operaciones de: suma, resta, multiplicacion y division
+    """
+
+    def __init__(self, operandoA, operandoB):
+        self.operandoA = operandoA
+        self. operandoB = operandoB
+
+    # Metodo para sumar
+    def sumar(self):
+        return self.operandoA + self.operandoB
+
+    def resta(self):
+        return self.operandoA - self.operandoB
+
+    def multiplicar(self):
+        return self.operandoA * self.operandoB
+
+    def dividir(self):
+        return self.operandoA / self.operandoB
+
+aritmetica1 = Aritmetica(8, 7) # Le pasamos los argumentos para los operandos
+print(f"La suma de los numeros es: {aritmetica1.sumar()}")
+print(f"La resta de los numeros es: {aritmetica1.resta()}")
+print(f"La multiplicacion de los numeros es: {aritmetica1.multiplicar()}")
+print(f"La division de los numeros es: {aritmetica1.dividir():.2f}")
+
